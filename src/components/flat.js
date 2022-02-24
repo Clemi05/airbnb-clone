ff43a96ffbbfe1937218b33f8698f0d4808c2b2a
Clement Azalbert <clementazalbert04@gmail.com>
2022-02-24 22:23:06 +0100

    added new component -> flat

---
+++ src/components/flat.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./flat.css";
+
+class Flat extends React.Component {
+  render() {
+    return (
+      <div className="card-flat">
+        <div className="card-flat-img"></div>
+        <div className="card-flat-title"></div>
+      </div>
+    );
+  }
+}
+
+export default Flat;
